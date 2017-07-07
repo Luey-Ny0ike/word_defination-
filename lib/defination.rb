780ccf1c933b116fd29289f9e152e08d1c175999
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 14:49:56 +0300

    Defined a method to save definations by pushing user input to the empty array

--- lib/defination.rb
+++ lib/defination.rb
@@ -11,4 +11,9 @@ class Defination
   define_singleton_method(:all) do
     @@definations
   end
+
+  define_method(:save) do
+    @@definations.push(self)
+  end
+  # self in this case is the input we will gather from the user
 end
