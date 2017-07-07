acbb2de03af27e5daf3aae1c219c663e4165579d
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 14:44:24 +0300

    Made an empty array which will store our definations Made a method to return all definations in our array

--- lib/defination.rb
+++ lib/defination.rb
@@ -1,9 +1,14 @@
 class Defination
+  @@definations = []
   define_method(:initialize) do |defination|
     @defination = defination
   end
 
   define_method(:defination) do
     @defination
+  end
+
+  define_singleton_method(:all) do
+    @@definations
   end
 end
