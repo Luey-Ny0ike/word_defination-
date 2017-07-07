2b4e4f9681842e8c31e65a06703af7c00bcb5a57
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 08:26:38 +0300

    Added a comment to explain what the id does and how it works

--- lib/word.rb
+++ lib/word.rb
@@ -1,11 +1,13 @@
 class Word
   @@words = []
   attr_reader(:word, :id)
+  # used the attribute reader to access the attributes passed as arguments for the
+  # initialize method
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
     @id = @@words.length.+1
-    # used the attribute reader to access the attributes passed as arguments for the
-    # initialize method
+    # called the .length method to add a numer by one and assingn it to a word
+    # the first word would be 1 the fifth 5 and so on
   end
 
   define_singleton_method(:all) do
