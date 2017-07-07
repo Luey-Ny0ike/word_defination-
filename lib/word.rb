c8bad209f2b6ce9a9b21e42fd940d58769ab1f06
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 07:43:25 +0300

    Added a class array to store our new words defined a class method .all to return data from the array

--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,13 @@
 class Word
+  @@words = []
   attr_reader(:word)
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
     # used the attribute reader to access the attributes passed as arguments for the
     # initialize method
+  end
+
+  define_singleton_method(:all)do
+    @@words
   end
 end
