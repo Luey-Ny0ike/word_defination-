cdc1bdc2a8edb71e7ae3cb694456fe506ebbf270
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 07:36:57 +0300

    Included a comment to explain what the attr_reader does

--- lib/word.rb
+++ lib/word.rb
@@ -2,5 +2,7 @@ class Word
   attr_reader(:word)
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
+    # used the attribute reader to access the attributes passed as arguments for the
+    # initialize method
   end
 end
