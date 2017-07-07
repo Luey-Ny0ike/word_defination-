01266b0067f68b0f65558afb9d231fbfda4be86a
lewis nyoike <nyoikelewis@gmail.com>
2017-07-07 11:45:21 +0300

    Made all necessary requests to return data from the server and post requests to add data to the server

---
+++ app.rb
@@ -0,0 +1,31 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/contact')
+
+get('/') do
+  erb(:index)
+end
+# the homepage which renders the index template
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+# shows you all the words present as we call the .all method here
+
+get('/words/new') do
+  erb(:word_form)
+end
+# renders a form that querry the user to input new words
+
+post('/words') do
+  word = params.fetch('word')
+  word = Word.new(word)
+  word.save
+  erb(:success)
+end
+# the first word variable is from what we fetch from the id in the form
+# the second word variable is what we assign a class.new function to add a new
+# word in our array then we save it
+# simple
